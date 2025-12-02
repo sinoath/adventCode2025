566d27744b6341b60d0c12483ed9ba0b3fc5832a
Abraxas <p.buciuni@protonmail.com>
2025-12-02 21:07:47 +0100

    day01: second riddle works properly

--- day01/second.py
+++ day01/second.py
@@ -1,4 +1,4 @@
-f = open('./test2.txt', 'r')
+f = open('./input1.txt', 'r')
 rows = []
 for line in f:
     rows.append((line[0], line[1:-1]))
@@ -7,32 +7,31 @@ counter = 0
 starting_point = 50
 
 
-def rotation(position, direction, steps):
+def rotationL(position, steps):
     global counter
-    if len(steps) > 2:
-        st = int(steps[-2:])
-        counter += int(steps[0:-2])
-    else:
-        st = int(steps)
-    if direction == 'R':
-        temp = position + st
-    else:
-        temp = 100 - st + position
-    if temp >= 100:
+    st = int(steps[-2:])
+    if (position - st) <= 0 and position != 0:
         counter += 1
-        print(direction, position)
-    return temp % 100
+    if len(steps) > 2:
+        counter += int(steps[0:-2])
+    return (100 + position - st) % 100
 
-# this block is to consider just the last 2 digits (if numbers are greater than 99)
-temp = '12345625'
-if len(temp) > 2:
-    print(int(temp[-2:]))
-    print(int(temp[0:-2]))
-else:
-    print(int(temp))
+
+def rotationR(position, steps):
+    global counter
+    st = int(steps[-2:])
+    if (position + st) >= 100:
+        counter += 1
+    if len(steps) > 2:
+        counter += int(steps[0:-2])
+    return (position + st) % 100
+
 
 for row in rows:
-    starting_point = rotation(starting_point, row[0], row[1])
-    if starting_point == 0:
-        counter += 1
+    direct = row[0]
+    moves = row[1]
+    if direct.capitalize() == 'R':
+        starting_point = rotationR(starting_point, moves)
+    else:
+        starting_point = rotationL(starting_point, moves)
 print("Number of zeroes: ", counter)
