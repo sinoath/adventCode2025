0775f28788067a0c471480c3430f6f9113502664
DebXabaras <p.buciuni@protonmail.com>
2025-12-04 13:33:15 +0100

    day02: function to check if first half and second half match

--- day02/main.py
+++ day02/main.py
@@ -5,6 +5,18 @@ for line in file:
 file.close()
 
 
+def match_split(id:str):
+    '''Check if the first half of a string is equal to the second half'''
+    half = len(id) / 2
+    first_half = id[:half]
+    second_half = id[half:]
+    return first_half == second_half
+
+
+# Variables
 id_ranges = content_of_file.split(',')
+maybe_faulty_ids = []
+faulty_ids = []
+sum_ids = 0
 
 print(id_ranges)
