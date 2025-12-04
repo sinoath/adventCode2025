6d32e16c22fa99039a4e4a8a335f631a26ccaa6f
DebXabaras <p.buciuni@protonmail.com>
2025-12-04 15:16:58 +0100

    day02: cleaned main.py file

--- day02/main.py
+++ day02/main.py
@@ -1,4 +1,5 @@
-file = open('./test1.txt', 'r')
+file = open('./input1.txt', 'r')
+# file = open('./test1.txt', 'r')
 content_of_file = ''
 for line in file:
     content_of_file = line[:-1]
@@ -7,7 +8,7 @@ file.close()
 
 def match_split(id:str):
     '''Check if the first half of a string is equal to the second half'''
-    half = len(id) / 2
+    half = len(id) // 2
     first_half = id[:half]
     second_half = id[half:]
     return first_half == second_half
