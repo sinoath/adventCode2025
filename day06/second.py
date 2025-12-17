78da2661aa2ef3ae0b5fb19e5ea754bbe12f9967
DebXabaras <p.buciuni@protonmail.com>
2025-12-17 08:02:36 +0100

    day06: column parsing working

--- day06/second.py
+++ day06/second.py
@@ -9,5 +9,27 @@ for el in content_of_file:
     horizontalList.append(el[:-1])
 operators = horizontalList[-1]
 horizontalList.pop(-1)
-print(horizontalList)
-print(operators)
+# print(horizontalList)
+# print(operators)
+op_positions = []
+for i in range(len(operators)):
+    if not operators[i].isspace():
+        op_positions.append(i)
+print(op_positions)
+testList = ['' for _ in range(len(operators))]
+# print(len(testList))
+# print(len(operators))
+# print(operators)
+# print(testList)
+firstLine = horizontalList[0]
+for el in horizontalList:
+    for i in range(len(firstLine)):
+        testList[i] += el[i]
+# print(testList)
+# print(testList[-1].strip() + '. ')
+ 
+for pos in op_positions:
+    if pos == op_positions[-1]:
+        pass
+    else:
+        pass
