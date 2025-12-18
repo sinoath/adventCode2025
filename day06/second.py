a1d612db2ef6bc8b3b0b8f7d28f583dd0d7b93c4
DebXabaras <p.buciuni@protonmail.com>
2025-12-18 10:16:02 +0100

    day06: working second part, was hard

--- day06/second.py
+++ day06/second.py
@@ -1,4 +1,4 @@
-with open('./test1.txt', 'r') as f:
+with open('./input1.txt', 'r') as f:
     content_of_file = []
     for line in f:
         content_of_file.append(line)
@@ -9,27 +9,45 @@ for el in content_of_file:
     horizontalList.append(el[:-1])
 operators = horizontalList[-1]
 horizontalList.pop(-1)
-# print(horizontalList)
-# print(operators)
 op_positions = []
+
 for i in range(len(operators)):
     if not operators[i].isspace():
         op_positions.append(i)
-print(op_positions)
-testList = ['' for _ in range(len(operators))]
-# print(len(testList))
-# print(len(operators))
-# print(operators)
-# print(testList)
+
+parsedCol = ['' for _ in range(len(operators))]
 firstLine = horizontalList[0]
+
 for el in horizontalList:
     for i in range(len(firstLine)):
-        testList[i] += el[i]
-# print(testList)
-# print(testList[-1].strip() + '. ')
- 
-for pos in op_positions:
-    if pos == op_positions[-1]:
-        pass
+        parsedCol[i] += el[i]
+
+for i in range(len(parsedCol)):
+    parsedCol[i] = parsedCol[i].strip()
+
+columns = [[op, []] for op in operators if not op.isspace()]
+
+j = 0
+for idx in range(len(parsedCol)):
+    if parsedCol[idx] == '':
+        j += 1
+        continue
+    columns[j][1].append(parsedCol[idx])
+
+totals = []
+for el in columns:
+    if el[0] == '+':
+        result = 0
+        for add in el[1]:
+            result += int(add)
+        totals.append(result)
     else:
-        pass
+        result = 1
+        for mul in el[1]:
+            result *= int(mul)
+        totals.append(result)
+
+granTotal = 0
+for el in totals:
+    granTotal += el
+print(granTotal)
