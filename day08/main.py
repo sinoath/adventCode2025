962450cb9ed286eed1f4b0e9b24466275979f1ae
DebXabaras <p.buciuni@protonmail.com>
2026-01-05 08:46:29 +0100

    day08: function to calculate distance between points

--- day08/main.py
+++ day08/main.py
@@ -6,28 +6,25 @@ with open('./test1.txt') as f:
 with open('./test1.txt') as f:
     conten_of_file = f.read()
 
-# print(conten_of_file)
-# print(junctions)
-# print(junctions[0].split(','))
-
-temp = []
-for t in junctions[0].split(','):
-    temp.append(int(t))
-
-print(temp)
-
 
 def parse_numbers(s) -> list:
+    '''Get a string of 2 number separated by a comma and return a list
+    with the three integer as elements'''
     coords = []
     temp = s.split(',')
-    print(temp)
     for t in temp:
         coords.append(int(t))
     return coords
 
 
 def get_distance(a, b) -> float:
-    return a / b
+    '''Calculate the Euclidean distance between two points in 3D space'''
+    square = ((b[0] - a[0]) ** 2) + ((b[1] - a[1]) ** 2) + ((b[2] - a[2]) ** 2)
+    return square**(1/2)
 
 
-print(parse_numbers(junctions[0]))
+coordinates = []
+for junction in junctions:
+    coordinates.append(parse_numbers(junction))
+
+print(coordinates)
